c978a7980e9f5cdb311d64e93ed10be983d3f814
Rostislav Melkumyan <rosti@sanity.io>
2026-03-17 11:09:33 +0100

    chore: remove stale components/radio_ui/include/fonts/fonts.h

--- components/radio_ui/include/fonts/fonts.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "lvgl.h"
-
-extern const lv_font_t geist_regular_14;
-extern const lv_font_t geist_regular_16;
-extern const lv_font_t geist_regular_20;
-extern const lv_font_t geist_medium_24;
-extern const lv_font_t geist_mono_medium_48;
